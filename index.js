52f38030882c5718b5400ec65fddd90027364e2e
Brian LeRoux <b@brian.io>
2019-08-12 14:20:26 -0700

    remove architect/data in favor of architect/functions

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 #!/usr/bin/env node
-var sandbox = require('@architect/sandbox')
-var chalk = require('chalk')
-var repl = require('repl')
+let arc = require('@architect/functions')
+let sandbox = require('@architect/sandbox')
+let chalk = require('chalk')
+let repl = require('repl')
 
-/**
- * create a repl to @tables defined in .arc
- */
-module.exports = function arcRepl(/*opts*/) {
+async function start() {
+  let server = repl.start({
+    prompt: chalk.green('#!/data> ')
+  })
+  server.context.data = await arc.tables()//require('@architect/data')
+  return server
+}
 
-  // starts the repl
-  function start() {
-    let server = repl.start({
-      prompt: chalk.green('#!/data> ')
-    })
-    // eslint-disable-next-line
-    server.context.data = require('@architect/data')
-    return server
-  }
-
+function cmd(/*opts*/) {
+  function fail(e) {throw e}
   let runInSandbox = process.env.NODE_ENV === 'testing'
   if (runInSandbox) {
     let db = sandbox.db.start(function done() {
-      start().on('exit', db.close)
+      start().then(server=> {
+        server.on('exit', db.close)
+      }).catch(fail)
     })
   }
   else {
-    start()
+    start().catch(fail)
   }
+}
+
+module.exports = cmd 
+
+// allow direct invoke
+if (require.main === module) {
+  (async function() {
+    await cmd(process.argv)
+  })();
 }
